928f0896f8f9efd4da801d9deefc23d2bd6991a3
osman <osmancibik.caliskan@gmail.com>
2020-12-18 23:00:04 +0300

    as

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -4,8 +4,8 @@ const User = require("../models/UserModel");
 
 exports.getProducts = async(req,res)=>{
     const products = await Product.find()
-    console.log("product-->",products)
-    res.status(200).json({products});
+    // console.log("product-->",products)
+    res.status(200).json({merhaba});
 }
 exports.getDetails = async(req,res)=>{
     const product = await Product.findById({ _id: req.params.id })
